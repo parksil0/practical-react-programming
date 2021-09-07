696de616046e28a36482b8020d53013e1d8db7cf
parksil0 <parksil00@naver.com>
2021-09-08 06:57:19 +0900

    reselect 없이 구현하기

--- redux-test/src/friend/container/FriendMain.js
+++ redux-test/src/friend/container/FriendMain.js
@@ -1,32 +1,58 @@
-import React, { useEffect, useReducer } from 'react';
-import store from '../../common/store';
 import { getNextFriend } from '../../common/mockData'
-import { addFriend } from '../state';
+import { addFriend, setAgeLimit, setShowLimit } from '../state';
 import FriendList from '../component/FriendList';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import NumberSelect from '../component/NumberSelect';
+import { MAX_AGE_LIMIT, MAX_SHOW_LIMIT } from '../common';
 
 export default function FriendMain() {
-  const [, forceUpdate] = useReducer(v => v + 1, 0);
-  useEffect(() => {
-    let prevFriends = store.getState().friend.friends;
-    const unsubscribe = store.subscribe(() => {
-      const friends = store.getState().friend.friends;
-      if (prevFriends !== friends) {
-        forceUpdate();
-      }
-      prevFriends = friends;
-    });
-    return () => unsubscribe();
-  }, [])
+  const [
+    ageLimit,
+    showLimit,
+    friendsWithAgeLimit,
+    friendsWithAgeShowLimit
+  ] = useSelector(state => {
+    const { friends, ageLimit, showLimit } = state.friend;
+    const friendsWithAgeLimit = friends.filter(
+      friend => friend.age <= ageLimit
+    );
+    return [
+      ageLimit,
+      showLimit,
+      friendsWithAgeLimit,
+      friendsWithAgeLimit.slice(0, showLimit)
+    ];
+  }, shallowEqual);
+
+  const dispatch = useDispatch();
   function onAdd() {
     const friend = getNextFriend();
-    store.dispatch(addFriend(friend));
+    dispatch(addFriend(friend));
   }
+
   console.log("FriendMain render");
-  const friends = store.getState().friend.friends;
+  
   return (
     <div>
       <button onClick={onAdd}>친구 추가</button>
-      <FriendList friends={friends} />
+      <NumberSelect
+        onChange={(v) => dispatch(setAgeLimit(v))}
+        value={ageLimit}
+        options={AGE_LIMIT_OPTIONS}
+        postfix="세 이하만 보기"
+      />
+      <FriendList friends={friendsWithAgeLimit} />
+
+      <NumberSelect
+        onChange={(v) => dispatch(setShowLimit(v))}
+        value={showLimit}
+        options={SHOW_LIMIT_OPTIONS}
+        postfix="명 이하만 보기 (연령 제한 적용)"
+      />
+      <FriendList friends={friendsWithAgeShowLimit} />
     </div>
   );
 }
+
+const AGE_LIMIT_OPTIONS = [15, 20, 25, MAX_AGE_LIMIT];
+const SHOW_LIMIT_OPTIONS = [2, 4, 6, MAX_SHOW_LIMIT];
--- redux-test/src/index.js
+++ redux-test/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TimelineMain from './timeline/container/TimelineMain';
 import FriendMain from './friend/container/FriendMain';
+import store from './common/store';
+import { Provider} from 'react-redux';
 
 ReactDOM.render(
-  <div>
-    <FriendMain />
-    <TimelineMain />
-  </div>,
+  <Provider store={store}>
+    <div>
+      <FriendMain />
+      <TimelineMain />
+    </div>
+  </Provider>,
   document.getElementById('root')
 );
